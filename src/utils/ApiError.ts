4a49e1eab1b0036279c4047f619c8c1533b5b828
Yashaswi Kohli <yashasvi1707@gmial.com>
2024-04-22 16:54:52 +0530

    Refactor code structure and add new utility classes

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,26 @@
+class ApiError extends Error {
+    statusCode: number;
+    data: any;
+    success: boolean;
+    errors: any[];
+  
+    constructor(
+      statusCode: number,
+      message = "Something went wrong",
+      errors: any[] = [],
+      stack = ""
+    ) 
+    {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+  
+        if (stack) this.stack = stack;
+        else Error.captureStackTrace(this, this.constructor);
+    }
+}
+  
+  export { ApiError };
